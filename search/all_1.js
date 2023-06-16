1da864fca7da2df9fd9b027386533a5a9db54c02
Tatiana Agarkova <82842675+tataro4ka-1540@users.noreply.github.com>
2023-06-16 16:03:33 +0300

    Add files via upload

---
+++ search/all_1.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['event_0',['Event',['../classfive_1_1_event.html',1,'five']]],
+  ['eventlogger_1',['EventLogger',['../classfive_1_1_event_logger.html',1,'five']]]
+];
---
+++ search/functions_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['_5f_5finit_5f_5f_0',['__init__',['../classfive_1_1_event.html#a24b259539c1bd9d8a0d4e7475aa59e7e',1,'five::Event']]]
+];
